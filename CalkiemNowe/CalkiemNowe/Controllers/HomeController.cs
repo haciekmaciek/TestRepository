ce4967b44aa4103e192e95fd7ca024b0989fd638
Lucjan Moczulski <lucjan.moczulski@totalizator.pl>
2018-10-29 17:03:37 +0100

    fasdfaadsfadsf

--- CalkiemNowe/CalkiemNowe/Controllers/HomeController.cs
+++ CalkiemNowe/CalkiemNowe/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace CalkiemNowe.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact pagetest.";
 
             return View();
         }
